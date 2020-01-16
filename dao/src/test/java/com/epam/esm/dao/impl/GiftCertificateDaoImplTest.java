80fd9aa4e1bbca4a70f80f068c098ea276600d45
Siarhei Shumilin <siarhei_shumilin@epam.com>
2020-01-16 21:13:41 +0300

    added test by exist certificates

--- dao/src/test/java/com/epam/esm/dao/impl/GiftCertificateDaoImplTest.java
+++ dao/src/test/java/com/epam/esm/dao/impl/GiftCertificateDaoImplTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
 
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = DaoConfig.class)
 @ActiveProfiles("set")
@@ -26,10 +27,60 @@ public class GiftCertificateDaoImplTest {
     private GiftCertificateMapperTest giftCertificateMapper;
 
     @Test
-    public void findAll() {
-        GiftCertificate giftCertificate= giftCertificateDao.findById(2);
+    public void testFindByDescription() {
+        List<GiftCertificate> expected = giftCertificateDao.findByDescription("1");
+        List<GiftCertificate> actual = jdbcTemplate.query("select certificates.id, certificates.name, certificates.description, certificates.price, certificates.create_date, certificates.last_update_date, certificates.duration, tags.id as tag_id, tags.name as tag_name from certificates inner join tags on certificates.tag_id=tags.id where certificates.description like ?", new Object[]{"1"}, giftCertificateMapper);
+        Assert.assertEquals(expected, actual);
 
-        GiftCertificate query = jdbcTemplate.queryForObject("select certificates.id, certificates.name, certificates.description, certificates.price, certificates.create_date, certificates.last_update_date, certificates.duration, tags.id as tag_id, tags.name as tag_name from certificates inner join tags on certificates.tag_id=tags.id where certificates.id = ?", new Object[]{2}, giftCertificateMapper);
-        Assert.assertEquals(giftCertificate, query);
+    }
+
+    @Test
+    public void testFindById() {
+        GiftCertificate expected = giftCertificateDao.findById(2);
+        GiftCertificate actual = jdbcTemplate.queryForObject("select certificates.id, certificates.name, certificates.description, certificates.price, certificates.create_date, certificates.last_update_date, certificates.duration, tags.id as tag_id, tags.name as tag_name from certificates inner join tags on certificates.tag_id=tags.id where certificates.id = ?", new Object[]{2}, giftCertificateMapper);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindByPartName() {
+        List<GiftCertificate> expected = giftCertificateDao.findByPartName("1");
+        List<GiftCertificate> actual = jdbcTemplate.query("select certificates.id, certificates.name, certificates.description, certificates.price, certificates.create_date, certificates.last_update_date, certificates.duration, tags.id as tag_id, tags.name as tag_name from certificates inner join tags on certificates.tag_id=tags.id where certificates.name like ?", new Object[]{"%" + "1" + "%"}, giftCertificateMapper);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindByTag() {
+        List<GiftCertificate> expected = giftCertificateDao.findByTag("1");
+        List<GiftCertificate> actual = jdbcTemplate.query("select certificates.id, certificates.name, certificates.description, certificates.price, certificates.create_date, certificates.last_update_date, certificates.duration, tags.id as tag_id, tags.name as tag_name from certificates inner join tags on certificates.tag_id=tags.id where tags.name = ?", new Object[]{"1"}, giftCertificateMapper);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIsExistsById(){
+        boolean expected = giftCertificateDao.isExistById(1);
+        Boolean actual = jdbcTemplate.queryForObject("SELECT EXISTS(SELECT id FROM certificates WHERE id = ?)", Boolean.class, 1);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIsExistsByTagName(){
+        boolean expected = giftCertificateDao.isExistByTagName("1");
+        Boolean actual = jdbcTemplate.queryForObject("SELECT EXISTS(SELECT certificates.name from certificates inner join tags on certificates.tag_id=tags.id where tags.name = ?)", Boolean.class, "1");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIsExistsByName() {
+        boolean expected = giftCertificateDao.isExistByName("1");
+        Boolean actual = jdbcTemplate.queryForObject("SELECT EXISTS(SELECT name FROM certificates WHERE name like ?)", Boolean.class, "1");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIsExistsDescription() {
+        Boolean expected = giftCertificateDao.isExistByDescription("1");
+        Boolean actual = jdbcTemplate.queryForObject("SELECT EXISTS(SELECT name FROM certificates WHERE description like ?)", Boolean.class, "1");
+        Assert.assertEquals(expected, actual);
     }
 }
