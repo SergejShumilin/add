6f16e16a9067aa0ac3cd6aa7106c6b666c1858ce
Siarhei Shumilin <siarhei_shumilin@epam.com>
2020-01-22 00:03:00 +0300

    Fixed realese method in connection pool

--- repository/src/main/java/com/epam/esm/connection/ConnectionPool.java
+++ repository/src/main/java/com/epam/esm/connection/ConnectionPool.java
@@ -5,15 +5,12 @@ import com.epam.esm.exception.DataAccessException;
 import javax.sql.DataSource;
 import java.lang.reflect.Proxy;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class ConnectionPool {
 
     private final DataSource driverDataSource;
     private static LinkedBlockingQueue<Connection> connectionQueue;
-    private static List<Connection> usedConnections = new ArrayList<>();
 
     public ConnectionPool(DataSource driverDataSource, int poolSize) throws DataAccessException {
         this.driverDataSource = driverDataSource;
@@ -21,11 +18,7 @@ public class ConnectionPool {
         try {
             for (int i = 0; i < poolSize; i++) {
                 Connection connectionOrigin = driverDataSource.getConnection();
-                ClassLoader classLoader = connectionOrigin.getClass().getClassLoader();
-                Class<?>[] interfaces = connectionOrigin.getClass().getInterfaces();
-                ConnectionInvocationHandler invocationHandler = new ConnectionInvocationHandler(connectionOrigin);
-                Connection proxyConnection = (Connection) Proxy.newProxyInstance(classLoader, interfaces, invocationHandler);
-                connectionQueue.offer(proxyConnection);
+                connectionQueue.offer(connectionOrigin);
             }
         } catch (SQLException e) {
             throw new DataAccessException(e.getMessage(), e);
@@ -36,26 +29,25 @@ public class ConnectionPool {
      * @return ProxyConnection from pool
      */
     public Connection getConnection() {
-        Connection connection = null;
+        Connection proxyConnection = null;
         try {
-            connection = connectionQueue.take();
-            usedConnections.add(connection);
+            Connection connection = connectionQueue.take();
+            ClassLoader classLoader = connection.getClass().getClassLoader();
+            Class<?>[] interfaces = connection.getClass().getInterfaces();
+            ConnectionInvocationHandler invocationHandler = new ConnectionInvocationHandler(connection);
+            proxyConnection = (Connection) Proxy.newProxyInstance(classLoader, interfaces, invocationHandler);
         } catch (InterruptedException e) {
-
+            Thread.currentThread().interrupt();
         }
-        return connection;
+        return proxyConnection;
     }
 
     /**
      * @param connection return connection to pool
      */
-    public static void releaseConnection(Connection connection) {
+    /*package private*/ static void releaseConnection(Connection connection) {
         try {
-            boolean contains = usedConnections.contains(connection);
-            if (contains) {
                 connectionQueue.put(connection);
-                usedConnections.remove(connection);
-            }
         } catch (InterruptedException e) {
 
         }
